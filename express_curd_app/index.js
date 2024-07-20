a675726a389ccd0c53f8d3a6dd8f2501b3315c35
vishal <vishalninaniya8949info@gmail.com>
2024-07-20 23:21:46 +0530

    add new async method to update users

--- express_curd_app/index.js
+++ express_curd_app/index.js
@@ -22,18 +22,33 @@ app.get("/users", (req, res) => {
 app.post("/create-user", (req, res) => {
   const { name, age } = req.body;
   const id = users.length + 1;
-  fs.writeFileSync(
+
+  fs.writeFile(
     path.join(__dirname, "users.json"),
     JSON.stringify([...users, { id, name, age }]),
-    "utf-8"
+    "utf-8",
+    (err, data) => {
+      if (data) console.log(data);
+      if (err) console.log(err);
+    }
   );
 
-  const allUsers = fs.readFileSync(path.join(__dirname, "users.json"), "utf-8");
-  console.log("updateUser ", allUsers);
-  res.json({
-    message: "User created successfully",
-    users: JSON.parse(allUsers),
+  fs.readFile(path.join(__dirname, "users.json"), "utf-8", (err, data) => {
+    if (err) console.log(err);
+    if (data) {
+      res.status(200).json({ status: "ok", users: JSON.parse(data) });
+    }
   });
+  // fs.writeFileSync(
+  //   path.join(__dirname, "users.json"),
+  //   JSON.stringify([...users, { id, name, age }]),
+  //   "utf-8"
+  // );
+  // const allUsers = fs.readFileSync(path.join(__dirname, "users.json"), "utf-8");
+  // res.json({
+  //   message: "User created successfully",
+  //   users: JSON.parse(allUsers),
+  // });
 });
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
